9dd3fcbe23759aa28884a6b2720349d89a05f7ac
Gabester0 <gceipper@gmail.com>
2020-12-21 22:11:46 -0500

    removed /build from gitignore

---
+++ build/precache-manifest.f6a52fa1204b7a5f5b092c33692dd83d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8243702ad20c7c4ac9a9b0fa93a31d17",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "713bf9d3b30b8ff777e1",
+    "url": "/TicTacToe/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "605180a244a035c179ac",
+    "url": "/TicTacToe/static/js/2.3983ac15.chunk.js"
+  },
+  {
+    "revision": "f89ec0e947fe52779183e6ebdf1dc0ff",
+    "url": "/TicTacToe/static/js/2.3983ac15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "713bf9d3b30b8ff777e1",
+    "url": "/TicTacToe/static/js/main.d810a773.chunk.js"
+  },
+  {
+    "revision": "ebdbc3f3c36f0e8a7009",
+    "url": "/TicTacToe/static/js/runtime-main.3e2f971d.js"
+  },
+  {
+    "revision": "e0c09975e35a7699c5b15d5e98ae4f70",
+    "url": "/TicTacToe/static/media/mute.e0c09975.svg"
+  },
+  {
+    "revision": "ed9300857243cb6dc1322699fb22cc93",
+    "url": "/TicTacToe/static/media/volume.ed930085.svg"
+  },
+  {
+    "revision": "fb07a03d0d8413001c79490897e5d463",
+    "url": "/TicTacToe/static/media/vs-pop-1.fb07a03d.mp3"
+  },
+  {
+    "revision": "e816b758229aa2c756d53098399e8e1a",
+    "url": "/TicTacToe/static/media/wood-click-1.e816b758.wav"
+  }
+]);
