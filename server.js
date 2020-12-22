50df489262c01517951436ecbbdb9549aac19d61
Gabester0 <gceipper@gmail.com>
2020-12-21 22:06:11 -0500

    updated server.js and changed procfile name to use capital P

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
-const express = require('express');
+const express = require("express");
+// eslint-disable-next-line no-unused-vars
+// const bodyParser = require('body-parser');
+const path = require("path");
 const app = express();
-app.use(express.static(__dirname + '/'));
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, "build")));
+
+// This route serves the React app
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
