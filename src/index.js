1d800901ea9eb08039ad79d1840e4e51e09be159
Gabester0 <gceipper@gmail.com>
2021-01-01 03:58:33 -0500

    second try with serviceWorker.js in src: registering serviceWorker in index.js instead of index.html because is also in src folder

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+if("serviceWorker" in navigator){
+  window.addEventListener("load", ()=>{
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res=> console.log("Service Worker registered"))
+      .catch(err=> console.log("Service worker not registered: ", err))
+  })
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
