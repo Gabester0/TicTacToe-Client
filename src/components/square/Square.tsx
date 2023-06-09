ada8ff757ca1d556519c2f03fd87dea30128167a
gabester0 <gceipper@gmail.com>
2023-06-08 22:23:46 -0400

    Converting Square to ts

---
+++ src/components/square/Square.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Player } from "../../types";
+import { SquareDiv } from "./SquareStyles";
+
+const Square = ({
+    id,
+    value,
+    click,
+}: {
+    id: string;
+    value: Player;
+    click: () => void;
+}): JSX.Element => {
+    return (
+        <SquareDiv className="square" id={id} onClick={click}>
+            {/* <p>{number}</p> */}
+            <h1>{value}</h1>
+        </SquareDiv>
+    );
+};
+
+export default Square;
