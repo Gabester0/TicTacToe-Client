5f95da48ae736472465e9b3a10ba8c684786c21d
gabester0 <gceipper@gmail.com>
2023-06-11 07:04:26 -0400

    finished everything except for the scary confetti cannon, started that too

--- src/components/confettiDot/ConfettiDotStyles.tsx
+++ src/components/confettiDot/ConfettiDotStyles.tsx
@@ -1,9 +1,9 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const StyledConfettiDotDiv = styled.svg`
     position: absolute;
     will-change: transform;
     pointer-events: none;
-    `;
-    // height: 10px;
-    // width: 10px;
+`;
+// height: 10px;
+// width: 10px;
