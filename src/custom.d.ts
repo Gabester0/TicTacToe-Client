81eb897ce322502e433ba6442cc33de6c971e93c
gabester0 <gceipper@gmail.com>
2023-06-13 20:02:46 -0400

    cleaning up

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,6 +1,2 @@
 declare module "*.svg";
 declare module "*.wav";
-
-// Write list of everything I fear about looking for  jobs
-// Everything I'm afraid could go wrong
-// Everything I'm afraid I could lose if I take the wrong job
